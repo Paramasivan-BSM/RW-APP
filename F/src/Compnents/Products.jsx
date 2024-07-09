ad81016f1c6218726a675fe39e8419ab9662e72a
Paramasivan <paramasivan645@gmail.com>
2024-07-09 17:11:55 +0000

    .

--- F/src/Compnents/Products.jsx
+++ F/src/Compnents/Products.jsx
@@ -1,52 +1,40 @@
-import "../Styles/Products.css"
+
+import "../Styles/Products.css";
+import MilkBottle from "../assets/MilkBottle.png";
 
 let BoxStyle = {
-
-minHeight:"200px",
-"min-width":"200px",
-backgroundColor:"whitesmoke",
-"box-shadow":"2px 2px 5px black"
-
-}
-
+    minHeight: "200px",
+    minWidth: "200px",
+    backgroundColor: "whitesmoke",
+    boxShadow: "2px 2px 5px black"
+};
 
 // Dynamic Data
-
-let ProductStock = ()=>{
+let ProductStock = () => {
     let Stock = 10;
     return Stock > 0 ? Stock : "Zero";
-}
+};
 
-
-export let Products = ()=>{
-    return(
+export let Products = () => {
+    return (
         <div className="row">
-
             <div className="col PC-1" style={BoxStyle}>
-
-                <h3 className="ProductTitle">FreshMilk</h3>
-                <p className="ProductDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <h5 className="ProductPrice">Price:$30</h5>
-
-
-                <div className="ProductStock">
-                <button className="Product_Add_Btn_1">+</button>
-                <span className="StockDetails">{ProductStock()}</span>
-                <button className="Product_Add_Btn_1">-</button>
+                <div className="row">
+                    <div className="col-10">
+                        <h3 className="ProductTitle">FreshMilk</h3>
+                        <p className="ProductDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <h5 className="ProductPrice">Price: $30</h5>
+                        <div className="ProductStock">
+                            <button className="Product_Add_Btn_1">+</button>
+                            <span className="StockDetails">{ProductStock()}</span>
+                            <button className="Product_Add_Btn_1">-</button>
+                        </div>
+                    </div>
+                    <div className="col-2">
+                        <img src={MilkBottle} alt="" className="ProductImage img-fluid"/>
+                    </div>
                 </div>
-
-                <img src={require()} alt="" srcset="" />
-
-               
-
-
             </div>
-
-
-
-
-
-
         </div>
-    )
-}
+    );
+};
